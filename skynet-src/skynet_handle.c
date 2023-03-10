9b2f1e9e6cc1da057d8707fa125ec57c48f3c15a
kevin.ouyang <460632034@qq.com>
2023-03-11 00:44:36 +0800

    commit skynet_handle comment

--- skynet-src/skynet_handle.c
+++ skynet-src/skynet_handle.c
@@ -11,56 +11,80 @@
 #define DEFAULT_SLOT_SIZE 4
 #define MAX_SLOT_SIZE 0x40000000
 
+ /*
+  * 服务名字结构体
+  * */
 struct handle_name {
-	char * name;
-	uint32_t handle;
+	char * name;		//	服务的名字
+	uint32_t handle;	// 服务的句柄ID
 };
 
 struct handle_storage {
-	struct rwlock lock;
+	struct rwlock lock;		// 因为可能多个线程进行服务操作，需要读写锁
 
-	uint32_t harbor;
-	uint32_t handle_index;
-	int slot_size;
+	uint32_t harbor;	//	当前节点的harbor
+	uint32_t handle_index;	// 当前节点的句柄自增ID
+	int slot_size;		// 哈希槽大小
+
+	// 实现一个哈希表 存储节点上的服务实例
+	// 这里是通过平坦地址的方式来解决哈希冲突
 	struct skynet_context ** slot;
 	
-	int name_cap;
-	int name_count;
-	struct handle_name *name;
+	int name_cap;	//	服务名字数组的容量
+	int name_count;	//	服务名字数组的大小
+	struct handle_name *name;	//	服务名字数组
 };
 
+// 维护节点的服务管理容器 定义成static变量
 static struct handle_storage *H = NULL;
 
 uint32_t
 skynet_handle_register(struct skynet_context *ctx) {
 	struct handle_storage *s = H;
 
-	rwlock_wlock(&s->lock);
+	rwlock_wlock(&s->lock);	// --%-- 添加写锁 --%--
 	
 	for (;;) {
 		int i;
 		uint32_t handle = s->handle_index;
+		// 通过遍历的方式找到当前哈希槽的空闲位置
+		// 这里用了一个取巧的方式，因为最差情况就是当前所有的位置都满了
+		// 遍历的次数所以最大业就是哈希槽的当前大小
 		for (i=0;i<s->slot_size;i++,handle++) {
+			// 如果handle超过了24位的大小（16777216） 则重新从1开始
+			// 一般单个节点内的服务数量不太可能超过这个数量级
 			if (handle > HANDLE_MASK) {
 				// 0 is reserved
 				handle = 1;
 			}
+			// 计算哈希值
 			int hash = handle & (s->slot_size-1);
 			if (s->slot[hash] == NULL) {
 				s->slot[hash] = ctx;
+
+				// 写入哈希槽成功以后再将ID自增
 				s->handle_index = handle + 1;
 
+				// --%-- 释放写锁 --%--
 				rwlock_wunlock(&s->lock);
 
+				// 合并harbor生成完整的句柄ID
 				handle |= s->harbor;
 				return handle;
 			}
 		}
+
+		// 没找到空闲的哈希槽位 则进行哈希槽扩充 然后rehash
+
+		// 哈希槽的最大长度不能超过节点的句柄最大值
 		assert((s->slot_size*2 - 1) <= HANDLE_MASK);
+		//在当前槽位大小的基础上翻倍扩充哈希槽
 		struct skynet_context ** new_slot = skynet_malloc(s->slot_size * 2 * sizeof(struct skynet_context *));
 		memset(new_slot, 0, s->slot_size * 2 * sizeof(struct skynet_context *));
+		// 对原有数据进行rehash
 		for (i=0;i<s->slot_size;i++) {
 			if (s->slot[i]) {
+				// 重算哈希值
 				int hash = skynet_context_handle(s->slot[i]) & (s->slot_size * 2 - 1);
 				assert(new_slot[hash] == NULL);
 				new_slot[hash] = s->slot[i];
@@ -77,18 +101,26 @@ skynet_handle_retire(uint32_t handle) {
 	int ret = 0;
 	struct handle_storage *s = H;
 
+	// 添加写锁
 	rwlock_wlock(&s->lock);
 
+	// 通过句柄找到服务实例对象
 	uint32_t hash = handle & (s->slot_size-1);
 	struct skynet_context * ctx = s->slot[hash];
 
 	if (ctx != NULL && skynet_context_handle(ctx) == handle) {
+		// 先从哈希槽中移除
 		s->slot[hash] = NULL;
 		ret = 1;
 		int i;
 		int j=0, n=s->name_count;
+
+		// 遍历名字数组移除对应handle的服务实例名字
+		// 这里有个取巧的方法 在遍历的过程中用j记录上次被释放的空间，下个数据直接放到上一个空位 这样保持数组的紧凑
 		for (i=0; i<n; ++i) {
+			// 从名字数组中找到对应的handle_name对象
 			if (s->name[i].handle == handle) {
+				// 释放名字字符串指针内存
 				skynet_free(s->name[i].name);
 				continue;
 			} else if (i!=j) {
@@ -96,15 +128,18 @@ skynet_handle_retire(uint32_t handle) {
 			}
 			++j;
 		}
+
 		s->name_count = j;
 	} else {
 		ctx = NULL;
 	}
 
+	// 释放写锁
 	rwlock_wunlock(&s->lock);
 
 	if (ctx) {
 		// release ctx may call skynet_handle_* , so wunlock first.
+		// 释放服务实例对象
 		skynet_context_release(ctx);
 	}
 
@@ -114,10 +149,14 @@ skynet_handle_retire(uint32_t handle) {
 void 
 skynet_handle_retireall() {
 	struct handle_storage *s = H;
+	// 不断的扫描哈希槽释放服务实例，直到哈希槽为空
 	for (;;) {
-		int n=0;
+		int n=0;  // 统计现有的服务实例数量
 		int i;
+
+		// 遍历整个服务实例哈希槽
 		for (i=0;i<s->slot_size;i++) {
+			// 添加读锁
 			rwlock_rlock(&s->lock);
 			struct skynet_context * ctx = s->slot[i];
 			uint32_t handle = 0;
@@ -125,6 +164,8 @@ skynet_handle_retireall() {
 				handle = skynet_context_handle(ctx);
 				++n;
 			}
+
+			// 释放读锁
 			rwlock_runlock(&s->lock);
 			if (handle != 0) {
 				skynet_handle_retire(handle);
@@ -140,15 +181,18 @@ skynet_handle_grab(uint32_t handle) {
 	struct handle_storage *s = H;
 	struct skynet_context * result = NULL;
 
+	// 添加读锁
 	rwlock_rlock(&s->lock);
 
 	uint32_t hash = handle & (s->slot_size-1);
 	struct skynet_context * ctx = s->slot[hash];
 	if (ctx && skynet_context_handle(ctx) == handle) {
 		result = ctx;
+		// ？？？
 		skynet_context_grab(result);
 	}
 
+	// 释放读锁
 	rwlock_runlock(&s->lock);
 
 	return result;
@@ -158,15 +202,19 @@ uint32_t
 skynet_handle_findname(const char * name) {
 	struct handle_storage *s = H;
 
+	// 添加读锁
 	rwlock_rlock(&s->lock);
 
 	uint32_t handle = 0;
 
 	int begin = 0;
 	int end = s->name_count - 1;
+
+	// 对名字结构数组进行二分查找
 	while (begin<=end) {
 		int mid = (begin+end)/2;
 		struct handle_name *n = &s->name[mid];
+		// 进行名字比较
 		int c = strcmp(n->name, name);
 		if (c==0) {
 			handle = n->handle;
@@ -179,6 +227,7 @@ skynet_handle_findname(const char * name) {
 		}
 	}
 
+	// 释放读锁
 	rwlock_runlock(&s->lock);
 
 	return handle;
@@ -187,6 +236,7 @@ skynet_handle_findname(const char * name) {
 static void
 _insert_name_before(struct handle_storage *s, char *name, uint32_t handle, int before) {
 	if (s->name_count >= s->name_cap) {
+		// 数量超过了容量 按原来的两倍扩容
 		s->name_cap *= 2;
 		assert(s->name_cap <= MAX_SLOT_SIZE);
 		struct handle_name * n = skynet_malloc(s->name_cap * sizeof(struct handle_name));
@@ -201,6 +251,7 @@ _insert_name_before(struct handle_storage *s, char *name, uint32_t handle, int b
 		s->name = n;
 	} else {
 		int i;
+		// before之后的后移一个位置 给要插入的数据腾出位置
 		for (i=s->name_count;i>before;i--) {
 			s->name[i] = s->name[i-1];
 		}
@@ -214,6 +265,7 @@ static const char *
 _insert_name(struct handle_storage *s, const char * name, uint32_t handle) {
 	int begin = 0;
 	int end = s->name_count - 1;
+	// 对名字结构数组进行二分查找，如果找到了对应名字的handle 说明同名的handle已经存在了
 	while (begin<=end) {
 		int mid = (begin+end)/2;
 		struct handle_name *n = &s->name[mid];
@@ -227,6 +279,7 @@ _insert_name(struct handle_storage *s, const char * name, uint32_t handle) {
 			end = mid - 1;
 		}
 	}
+	// 进行字符串拷贝
 	char * result = skynet_strdup(name);
 
 	_insert_name_before(s, result, handle, begin);
@@ -248,17 +301,25 @@ skynet_handle_namehandle(uint32_t handle, const char *name) {
 void 
 skynet_handle_init(int harbor) {
 	assert(H==NULL);
+	// 初始化服务存储器全局变量
 	struct handle_storage * s = skynet_malloc(sizeof(*H));
+	// 设置哈希槽的默认大小
 	s->slot_size = DEFAULT_SLOT_SIZE;
+	// 初始化哈希槽
 	s->slot = skynet_malloc(s->slot_size * sizeof(struct skynet_context *));
 	memset(s->slot, 0, s->slot_size * sizeof(struct skynet_context *));
 
+	// 初始化读写锁
 	rwlock_init(&s->lock);
 	// reserve 0 for system
+	// 初始化harbor值 这里harbor值已经进行了移位操作
 	s->harbor = (uint32_t) (harbor & 0xff) << HANDLE_REMOTE_SHIFT;
 	s->handle_index = 1;
+	// 名字结构数组默认容量是2
 	s->name_cap = 2;
 	s->name_count = 0;
+
+	// 初始化名字结构数组
 	s->name = skynet_malloc(s->name_cap * sizeof(struct handle_name));
 
 	H = s;
--- skynet-src/skynet_handle.h
+++ skynet-src/skynet_handle.h
@@ -4,19 +4,26 @@
 #include <stdint.h>
 
 // reserve high 8 bits for remote id
-#define HANDLE_MASK 0xffffff
-#define HANDLE_REMOTE_SHIFT 24
+#define HANDLE_MASK 0xffffff // C里面定义整数的长度为啥喜欢用16进制？
+#define HANDLE_REMOTE_SHIFT 24  //  handleID 高8位是harbor，低24位是本节点内的handle自增ID
 
 struct skynet_context;
 
+// 注册服务实例到handle管理器
 uint32_t skynet_handle_register(struct skynet_context *);
+// 回收句柄对应的服务实例对象
 int skynet_handle_retire(uint32_t handle);
+// 根据句柄获得服务实例对象
 struct skynet_context * skynet_handle_grab(uint32_t handle);
+// 回收所有的服务实例对象
 void skynet_handle_retireall();
 
+// 根据服务名字查找
 uint32_t skynet_handle_findname(const char * name);
+
 const char * skynet_handle_namehandle(uint32_t handle, const char *name);
 
+// 节点服务handle管理器初始化
 void skynet_handle_init(int harbor);
 
 #endif
